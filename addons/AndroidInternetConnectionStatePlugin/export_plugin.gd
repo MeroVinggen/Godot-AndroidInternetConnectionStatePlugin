8120400a9e4b7065bc2719084960fa3f6a837c99
Sandra Bullock <Sandra@Bullock>
2024-05-13 20:55:42 +0300

    autoload setup now only on Android

--- addons/AndroidInternetConnectionStatePlugin/export_plugin.gd
+++ addons/AndroidInternetConnectionStatePlugin/export_plugin.gd
@@ -9,11 +9,17 @@ var export_plugin: AndroidInternetConnectionStateClass
 func _enter_tree():
 	export_plugin = load("res://addons/AndroidInternetConnectionStatePlugin/AndroidInternetConnectionStateClass.gd").new(_plugin_name)
 	add_export_plugin(export_plugin)
-	add_autoload_singleton(_plugin_name, "res://addons/AndroidInternetConnectionStatePlugin/export_plugin.gd")
+	
+	if OS.get_name() == "Android":
+		add_autoload_singleton(_plugin_name, "res://addons/AndroidInternetConnectionStatePlugin/export_plugin.gd")
+	else:
+		push_warning("AndroidInternetConnectionStatePlugin is available only at Android platform, the autoload has not beed registered")
 
 
 func _exit_tree():
 	remove_export_plugin(export_plugin)
 	export_plugin = null
-	remove_autoload_singleton(_plugin_name)
+	
+	if ProjectSettings.has_setting("autoload/" + _plugin_name):
+		remove_autoload_singleton(_plugin_name)
 
