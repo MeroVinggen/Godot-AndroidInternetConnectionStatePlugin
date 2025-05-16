30372c361fa0ba1fe11882714fe9eecf8da1bbc4
Sandra Bullock <Sandra@Bullock>
2025-05-16 19:30:27 +0300

    updated example

--- example/test.gd
+++ example/test.gd
@@ -2,12 +2,22 @@ extends MarginContainer
 
 @onready var stateValueLabel: Label = $VBoxContainer/HBoxContainer/StateValueLabel
 @onready var subscriptionState: Label = $VBoxContainer/HBoxContainer3/SubscriptionState
+@onready var stateIndicator: Panel = $VBoxContainer/HBoxContainer4/StateIndicator
 
 var _isSubscribed: bool = false
 
 
 func updateInternetConnectionState(state: bool = AndroidNetworkStateNode.hasNetwork()):
 	stateValueLabel.text = str(state)
+	
+	var styleBox: StyleBoxFlat = stateIndicator.get_theme_stylebox("panel")
+	
+	if state:
+		styleBox.bg_color = Color.LIME
+	else:
+		styleBox.bg_color = Color.TOMATO
+		
+	stateIndicator.add_theme_stylebox_override("panel", styleBox)
 
 
 func _onSubscribeButtonPressed() -> void:
